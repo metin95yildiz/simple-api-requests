97f704516e8e22617b8c7d1404d2e4e53c2d72fc
metin95yildiz <metin_y1995@hotmail.com>
2021-08-25 12:00:05 +0300

    Added changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [fetchData, setData] = useState("");
+  const [addingNewPost, setNewPost] = useState(false);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+    .then(data => setData(data));
+  }, [fetchData]);
+  function deletePost(id) {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+        if (!response.ok) {
+          throw Error(response.status);
+         }
+         return response.json();
+        })
+  }
+  function editPost(id){
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+        if (!response.ok) {
+          throw Error(response.status);
+         }
+         return response.json();
+        })
+  }
+  function comment(id) {
+    fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+        if (!response.ok) {
+          throw Error(response.status);
+         }
+         return response.json();
+        })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="post-list">
+          <div>
+            <h1>Posts</h1>
+            <button onClick={() => setNewPost(true)}>Add new post</button>
+          </div>
+          <ul>
+              {
+                Object.values(fetchData).map(data => {
+                  return (
+                  <li key={data.id}>
+                      <h2>{data.title}</h2>
+                      <p>{data.body}</p>
+                      <div>
+                        <button  className="comments" onClick={() => comment(data.id)}>Comments</button>
+                        <button className="edit" onClick={() => editPost(data.id)}>Edit</button>
+                        <button className="delete" onClick={() => deletePost(data.id)}>Delete</button>
+                      </div>
+                  </li>
+                  )
+                })
+              }
+          </ul>
+        </div>
+        {
+          addingNewPost ?
+          <form action="https://jsonplaceholder.typicode.com/posts" method="POST">
+          <h2>ADD POST</h2>
+          <label>
+              <span>Title:</span>
+              <input type="text" name="title" required/><br/>
+          </label>
+          <br/>
+          <label>
+              <span>Body:</span>
+              <input type="text" name="body" required/><br/>
+          </label>
+          <button type="submit">
+            ADD POST
+          </button>
+        </form>
+        : null
+        }
+      </main>
     </div>
   );
 }
